6c3f1ec2041782076598ccfed131d70c183c6e31
Mahmoud Mosaad <mahmoudmosaad50@gmail.com>
2025-08-21 22:26:35 +0300

    Add swagger configuration

---
+++ src/main/java/com/demo/employee/config/SwaggerConfig.java
@@ -0,0 +1,28 @@
+package com.demo.employee.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Employee Management API")
+                        .description("Employee System Demo - Suez Canal Bank")
+                        .version("1.0"))
+                // Add Security Requirement
+                .addSecurityItem(new SecurityRequirement().addList("basicAuth"))
+                .components(new io.swagger.v3.oas.models.Components()
+                        .addSecuritySchemes("basicAuth",
+                                new SecurityScheme()
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .scheme("basic")));
+    }
+}
